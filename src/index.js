0e3dcabade0812d16577880e0c330a848cc47b6f
Lizette Pérez <lizette.perezcorrea@gmail.com>
2023-03-11 20:12:11 -0300

    Update index.js
    Funcion validar tarjeta, obligar al usuario a ingresar numero y solo obligar a ingresar numeros a traves de alert

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+function validarTarjeta(){
+    var tarjeta = document.getElementById('tarjeta').value;
+    if (tarjeta == ''){
+        alert('Por favor ingrese un número de tarjeta.');
+        return;
+    }
+
+    if (isNaN(tarjeta)) {
+        alert('Ingrese solo números en el campo de número de tarjeta.');
+        return;
+    }
+}

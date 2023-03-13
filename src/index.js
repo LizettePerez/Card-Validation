048eb2e412fcc7ab46cbadc561389d2383744695
Lizette Pérez <lizette.perezcorrea@gmail.com>
2023-03-13 15:17:18 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,55 +1,5 @@
-// import validator from './validator.js';
+import validator from './validator.js';
 
-// console.log(validator);
-
-function handleValidation() {
-    // Obtener los valores de los campos
-    const creditCardNumber = document.getElementById("creditCardNumber").value;
-    const creditCardName = document.getElementById("creditCardName").value;
-    const creditCardDateMonth = document.getElementById("creditCardDateMonth").value;
-    const creditCardDateYear = document.getElementById("creditCardDateYear").value;
-    const creditCardCvv = document.getElementById("creditCardCvv").value;
-
-    // Verificar que todos los campos estén completos
-    if (creditCardNumber === "" || creditCardName === "" || creditCardDateMonth === "" || creditCardDateYear === "" || creditCardCvv === "") {
-        document.getElementById("output").innerHTML = "Por favor complete todos los campos.";
-        return;
-    }
-
-    //Verificar que tenga al menos 11 digitos
-    if (creditCardNumber.length <= 11) {
-        alert("El número de tarjeta de crédito debe tener al menos 11 dígitos.");
-        return false;
-    }
-
-    // Validar el número de tarjeta de crédito
-    if (!validateCreditCardNumber(creditCardNumber)) {
-        document.getElementById("output").innerHTML = "La tarjeta de crédito " + creditCardNumber + " no es válida.";
-        return false;
-    }
-
-    // Si llegamos hasta aquí, todos los campos son válidos
-    document.getElementById("output").innerHTML = "La tarjeta de crédito " + creditCardNumber + " si es válida.";
-    
-}
-
-function validateCreditCardNumber(creditCardNumber) {
-    const trimmedNumber = document.getElementById("creditCardNumber").value;
-
-
-    // Implementar el algoritmo de Luhn
-    let sum = 0;
-    let shouldDouble = false;
-    for (let i = trimmedNumber.length - 1; i >= 0; i--) {
-        let digit = parseInt(trimmedNumber.charAt(i), 10);
-        if (shouldDouble) {
-            digit *= 2;
-            if (digit > 9) {
-                digit -= 9;
-            }
-        }
-        sum += digit;
-        shouldDouble = !shouldDouble;
-    }
-    return (sum % 10) === 0;
-}
+/* eslint-disable no-console */
+console.log(validator);
+/* eslint-enable no-console */
--- src/validator.js
+++ src/validator.js
@@ -1,4 +1,26 @@
-// const validator = {
-// };
+const validator = {
+  isValid: function(creditCardNumber) {
+    let sum = 0;
+    let shouldDouble = false;
+    for (let i = creditCardNumber.length - 1; i >= 0; i--) {
+      let digit = parseInt(creditCardNumber.charAt(i));
+      if (shouldDouble) {
+        digit *= 2;
+        if (digit > 9) {
+          digit -= 9;
+        }
+      }
+      sum += digit;
+      shouldDouble = !shouldDouble;
+    }
+    return (sum % 10) === 0;
+  },
 
-// export default validator;
+  maskify: function (creditCardNumber) {
+    const lastFourDigits = creditCardNumber.substr(-4);
+    const maskedDigits = creditCardNumber.substr(0, creditCardNumber.length - 4).replace(/./g, "#");
+    return maskedDigits + lastFourDigits;
+  }
+};
+
+export default validator;

4c447096fe1b0c3a70b504c9d2bf812d29d49b50
Lizette Pérez <lizette.perezcorrea@gmail.com>
2023-03-15 17:58:46 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import validator from './validator.js';
 
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Evita que se envíe el formulario
+
+    const creditCardNumber = form.querySelector('#creditCardNumber').value;
+    /* eslint-disable no-console */
+    console.log(creditCardNumber);
+    /* eslint-enable no-console */
+
+
+    if (validator.isValid(creditCardNumber)) {
+      const maskedNumber = validator.maskify(creditCardNumber);
+      alert(`La tarjeta ${maskedNumber} es válida.`);
+    } else {
+      alert('El número de tarjeta no es válido.');
+    }
+  });
+});
+
+
 /* eslint-disable no-console */
 console.log(validator);
 /* eslint-enable no-console */
